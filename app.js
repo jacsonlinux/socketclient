253071882055f90a927f80ae9dd43615cc0b32b5
Jacson S. Correa <jacsonlinux@gmail.com>
2020-02-21 13:50:58 -0300

    Get dynamic data Implemented

--- app.js
+++ app.js
@@ -1,35 +1,47 @@
 const si = require('systeminformation');
 const WebSocketClient = require('websocket').client;
 const client = new WebSocketClient();
-
-const IP_SERVER = '...';
-const PORT = '...';
-
-function getDataSystem() {
-    console.log('Get data system...');
-    return si.getStaticData()
-        .then(data => data )
-        .catch(error => console.error(error));
-}
-
-client.connect(`ws://${IP_SERVER}:${PORT}/`, 'echo-protocol');
+const IP_SERVER = '192.168.1.217';
+const PORT = '1983';
+let UUID;
 
 client.on('connectFailed', error => {
     console.log('Connect Error: ' + error.toString());
 });
-
 client.on('connect', connection => {
-    console.log('WebSocket Client Connected.');
-    getDataSystem()
-        .then(res => {
-            connection.sendUTF(JSON.stringify(res));
-            console.log('Data sent to server...');
-        })
-        .catch(err => err.message);
+    console.log('WebSocket Client Connected');
     connection.on('error', error => {
         console.log("Connection Error: " + error.toString());
     });
     connection.on('close', () => {
         console.log('echo-protocol Connection Closed');
     });
+    function getDataSystem() {
+        if (connection.connected) {
+            console.log('Get static data system...');
+            return si.getStaticData()
+                .then(data => {
+                    UUID = data.system.uuid;
+                    data.type = 'static';
+                    connection.sendUTF(JSON.stringify(data));
+                    setTimeout(getDataSystemDynamic, 1000);
+                } )
+                .catch(error => console.error(error));
+        }
+    }
+    function getDataSystemDynamic() {
+        if (connection.connected) {
+            console.log('Get dynamic data system...');
+            return si.getDynamicData()
+                .then(data => {
+                    data.uuid = UUID;
+                    data.type = 'dynamic';
+                    connection.sendUTF(JSON.stringify(data));
+                    setTimeout(getDataSystemDynamic, 1000);
+                } )
+                .catch(error => console.error(error));
+        }
+    }
+    getDataSystem();
 });
+client.connect(`ws://${IP_SERVER}:${PORT}/`, 'echo-protocol');
